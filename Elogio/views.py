730107d288fd3bc389c0953cabe9bf251e7897c8
Gabriel Luiz <gabriells760@gmail.com>
2020-05-05 18:15:28 -0300

    Commit login
    Commit login

--- Elogio/views.py
+++ Elogio/views.py
@@ -28,8 +28,11 @@ def logout(request):
 
 
 def login(request):
+    template_name = 'Elogio/index.html'
     if request.method != 'POST':
         return render(request, 'Elogio/login.html')
+    if request.user.is_authenticated:
+        return render(request, template_name)
 
     usuario = request.POST.get('usuario')
     senha = request.POST.get('senha')
