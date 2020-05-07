a8cd2fc35a3b32987b371306438c5b5275a9b71a
Gabriel Luiz <gabriells760@gmail.com>
2020-05-07 17:20:18 -0300

    Commit c
    c

--- templates/static/js/elogio.js
+++ templates/static/js/elogio.js
@@ -1,7 +1,7 @@
 function gerarElogio()
 {
      var min = Math.ceil(1);
-    var max = Math.floor(21);
+    var max = Math.floor(16);
     var a = Math.floor(Math.random() * (max - min)) + min;
 
        console.log(a);
@@ -11,77 +11,57 @@ function gerarElogio()
     }
     if(a == 2)
     {
-        document.getElementById("elogios_p").innerHTML = "Você é sexy, é sensual, seu corpo é lindo, ele inteiro é lindo, eu amo seu corpo";
+        document.getElementById("elogios_p").innerHTML = "Seu beijo é incrivel, é o melhor beijo do mundo, só sou feliz bjando sua boca";
     }
     if(a == 3)
     {
-        document.getElementById("elogios_p").innerHTML = "Seu beijo é incrivel, é o melhor beijo do mundo, só sou feliz bjando sua boca";
+        document.getElementById("elogios_p").innerHTML = "Seus olhos são lindos demais, eu ficaria o dia inteiro olhando para eles sem me cansar. Deveriam ser considerados uma das 7 maravilhas do mundo";
     }
     if(a == 4)
     {
-        document.getElementById("elogios_p").innerHTML = "Seus olhos são lindos demais, eu ficaria o dia inteiro olhando para eles sem me cansar. Deveriam ser considerados uma das 7 maravilhas do mundo";
+        document.getElementById("elogios_p").innerHTML = "Você me faz ser muito feliz, me faz feliz, como eu nunca pensei que poderia ser, nem nos meus melhores sonhos eu podia imaginar que seria tão feliz quanto sou ao seu lado";
     }
     if(a == 5)
     {
-        document.getElementById("elogios_p").innerHTML = "Você me faz ser muito feliz, me faz feliz, como eu nunca pensei que poderia ser, nem nos meus melhores sonhos eu podia imaginar que seria tão feliz quanto sou ao seu lado";
+        document.getElementById("elogios_p").innerHTML = "Seu sorriso é uma das coisas mais lindas desse mundo, deveria ser considerado a cura para a tristeza, pq sério, só de ver seu sorriso já faz o dia de qualquer pessoa valer a pena";
     }
     if(a == 6)
     {
-        document.getElementById("elogios_p").innerHTML = "Sua bunda é linda demais, tipo muito mesmo, você sabe que eu amo apertar ela né? Então é pq ela é realmente maravilhosa, linda para caralho, consegue te deixar mais sexy ainda";
+        document.getElementById("elogios_p").innerHTML = "Você é inteligente demais, muito mesmo, eu fico impressionado com o quanto você é, com a quantidade de coisas que você sabe, com a capacidade de entender as coisas dificeis que você tem, você incrivel pqp";
     }
     if(a == 7)
     {
-        document.getElementById("elogios_p").innerHTML = "Seus peitos são lindos demais, seu corpo inteiro é lindo aliás, eu amo cada pedacinho dele";
+        document.getElementById("elogios_p").innerHTML = "Seu jeito de ser é incrivel, eu me apaixonei por você e me apaixono todos os dias, cada maniazinha, cada olhar, cada sorriso, seu jeitinho de falar, tudo faz eu te amar ainda mais";
     }
     if(a == 8)
     {
-        document.getElementById("elogios_p").innerHTML = "Seus peitos são lindos demais, seu corpo inteiro é lindo aliás, eu amo cada pedacinho dele";
+        document.getElementById("elogios_p").innerHTML = "Seu abraço acho que é capaz de ressucitar alguém de tão bom que é, esqueço de todos os problemas quando estou te abraçando";
     }
     if(a == 9)
     {
-        document.getElementById("elogios_p").innerHTML = "Seu sorriso é uma das coisas mais lindas desse mundo, deveria ser considerado a cura para a tristeza, pq sério, só de ver seu sorriso já faz o dia de qualquer pessoa valer a pena";
+        document.getElementById("elogios_p").innerHTML = "Seu cabelo é lindo, consegue te deixar mais linda do que já é, não sei elogiar cabelo muito bem pq não conheço muito de vabelo ofdisahufa mas sou apaixonado por ele também";
     }
     if(a == 10)
     {
-        document.getElementById("elogios_p").innerHTML = "Você é inteligente demais, muito mesmo, eu fico impressionado com o quanto você é, com a quantidade de coisas que você sabe, com a capacidade de entender as coisas dificeis que você tem, você incrivel pqp";
+        document.getElementById("elogios_p").innerHTML = "Estar com você é incrivel, tão incrivel que quero estar minha vida inteira com você, morar com você, acordar todos os dias ao seu lado, pq só estou completo quando estou com você!";
     }
     if(a == 11)
     {
-        document.getElementById("elogios_p").innerHTML = "Seu jeito de ser é incrivel, eu me apaixonei por você e me apaixono todos os dias, cada maniazinha, cada olhar, cada sorriso, seu jeitinho de falar, tudo faz eu te amar ainda mais";
+        document.getElementById("elogios_p").innerHTML = "Você engraçada (mesmo eu sendo mais), seu senso de humor é algo incrivel para mim, o seu sarcasmo, você zoar as pessoas, é algo engraçado demais";
     }
     if(a == 12)
     {
-        document.getElementById("elogios_p").innerHTML = "Seu abraço acho que é capaz de ressucitar alguém de tão bom que é, esqueço de todos os problemas quando estou te abraçando";
+        document.getElementById("elogios_p").innerHTML = "Você é esforçada, estuda para caralho, sempre estudou, e é capaz de muitoooo, irá atingir todos os seus objetivos pela sua capacidade, inteligencia e esforço, isso é algo maravilhoso em você!";
     }
     if(a == 13)
     {
-        document.getElementById("elogios_p").innerHTML = "Seu cabelo é lindo, consegue te deixar mais linda do que já é, não sei elogiar cabelo muito bem pq não conheço muito de vabelo ofdisahufa mas sou apaixonado por ele também";
+        document.getElementById("elogios_p").innerHTML = "Você é forte demais, você é muito forte mesmo, luta e vence suas batalhas, mesmo elas sendo dificeis, e saiba que nunca estará sozinha, sempre estarei com você!";
     }
     if(a == 14)
     {
-        document.getElementById("elogios_p").innerHTML = "Estar com você é incrivel, tão incrivel que quero estar minha vida inteira com você, morar com você, acordar todos os dias ao seu lado, pq só estou completo quando estou com você!";
-    }
-    if(a == 15)
-    {
-        document.getElementById("elogios_p").innerHTML = "Estar com você é incrivel, tão incrivel que quero estar minha vida inteira com você, morar com você, acordar todos os dias ao seu lado, pq só estou completo quando estou com você!";
-    }
-    if(a == 16)
-    {
-        document.getElementById("elogios_p").innerHTML = "Você engraçada (mesmo eu sendo mais), seu senso de humor é algo incrivel para mim, o seu sarcasmo, você zoar as pessoas, é algo engraçado demais";
-    }
-    if(a == 17)
-    {
-        document.getElementById("elogios_p").innerHTML = "Você é esforçada, estuda para caralho, sempre estudou, e é capaz de muitoooo, irá atingir todos os seus objetivos pela sua capacidade, inteligencia e esforço, isso é algo maravilhoso em você!";
-    }
-    if(a == 18)
-    {
-        document.getElementById("elogios_p").innerHTML = "Você é forte demais, você é muito forte mesmo, luta e vence suas batalhas, mesmo elas sendo dificeis, e saiba que nunca estará sozinha, sempre estarei com você!";
-    }
-    if(a == 19)
-    {
         document.getElementById("elogios_p").innerHTML = "Você é incrivel, simplesmente incrivel, em todos os sentidos da vida, você é demais, eu admiro você mais do que já admirei qualquer outro, tenho orgulho de você!";
     }
-    if(a == 20)
+    if(a == 15)
     {
         document.getElementById("elogios_p").innerHTML = "Por todos os elogios que já te fiz, por tudo o que vc é, por todos os elogios que escrevi aqui, você é o amor da minha vida!";
     }
